c9158ccb52985f024301f8d112cf52ce90c6bc3e
celoazevedo <meazevedo86@gmail.com>
2020-04-10 17:20:27 -0700

    fixed

--- lesson1.js
+++ lesson1.js
@@ -1,10 +1,11 @@
 //Should console.log all names in the name array
 
-function printArray (array){
+
 let names = ["pedro", "john"]
+function printArray (array){
 for (var i = 0; i < names.length; i++) {
     console.log("My Name is " + names[i])
 }
 }
 
-printArray(names);
+printArray();
